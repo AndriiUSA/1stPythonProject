d391f38f7928dd4467010259abd377e803bd46db
Andrii Vlezko <vlezkoUSA@gmail.com>
2023-07-26 16:20:03 -0400

    update part 5

--- Zlatopolsriy_part5/main.py
+++ Zlatopolsriy_part5/main.py
@@ -29,13 +29,64 @@
 # for i in range(a,b):
 #     print(i)
 
-# 5.9. Напечатать таблицу перевода расстояний в дюймах в сантиметры для значений 10, 11, ..., 22 дюйма (1 дюйм = 25,4 мм).
+#5.9. Напечатать таблицу перевода расстояний в дюймах в сантиметры для значений 10, 11, ..., 22 дюйма (1 дюйм = 25,4 мм).
 # for inch in range(10,23):
 #     cm = inch * 2.54
 #     print(f'{inch} inches = {cm} centimeters')
 
-# 5.10 Напечатать таблицу перевода 1, 2, ... 20 долларов США в грн по текущему курсу (значение курсас клавиатуры).
-exch = float(input('Type exchange rate uah to usd: '))
-for usd in range(21):
-    uah = usd * exch
-    print(f'{usd} us dollars = {uah} ukrainian hryvnia')
+#5.10 Напечатать таблицу перевода 1, 2, ... 20 долларов США в грн по текущему курсу (значение курсас клавиатуры).
+# exch = float(input('Type exchange rate uah to usd: '))
+# for usd in range(1,21):
+#     uah = usd * exch
+#     print(f'{usd} us dollars = {uah} ukrainian hryvnia')
+
+#5.14. Напечатать таблицу умножения на 9:
+# for numb in range(1,10):
+#     mult = 9 * numb
+#     print(f' 9 x {numb} = {mult}')
+
+# 5.24. Вывести «столбиком» следующие числа: 3,2, 3,2, 3,3, ...,3,9.
+# i = 3.2
+# while i < 4.0:
+#     print(round(i,2))
+#     i += 0.1
+
+# 5.25. Вывести «столбиком» следующие числа: 2,2, 2,4, 2,6, ...,4,2.
+# n = 2.2
+# while n < 4.3:
+#     print(round(n,1))
+#     n +=0.1
+#
+# 5.26. Вывести «столбиком» следующие числа: 4,4, 4,6, 4,8, ...,6,4.
+# numb = 4.4
+# while numb < 6.5:
+#     print(round(numb,1))
+#     numb +=0.1
+
+# # 5.27. Найти: а) сумму всех целых чисел от 200 до 600;
+# sum_of_numbers = 0
+# for num in range(200, 600 + 1):
+#     sum_of_numbers += num
+# print("Sum of numbers in the range:", sum_of_numbers)
+
+# б) сумму всех целых чисел от a до 400 (значение a вводится с клавиатуры; a ≤ 400);
+# sum_of_numbers = 0
+# a = int(input('Type "a" value (should be less or equals 400): '))
+# for num in range(a, 400 + 1):
+#     sum_of_numbers += num
+# print("Sum of numbers in the range:", sum_of_numbers)
+
+# 5.28. Найти: а) произведение всех целых чисел от 7 до 14;
+# product_of_numbers = 1
+# for n in range(7,15):
+#     print(n)
+#     product_of_numbers *= n
+# print(product_of_numbers)
+
+# 5.30. Найти: а) сумму кубов всех целых чисел от 25 до 40;
+sum_of_cube = 0
+for n in range(25,40):
+    cube = n**3
+    sum_of_cube += cube
+    print(cube)
+print(sum_of_cube)
